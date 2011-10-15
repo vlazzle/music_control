bd010f047b3b9e5b7ee61a1cedffd3eb60d69e72
vlad <iamvlad@gmail.com>
2011-10-14 18:08:58 -0700

    first

---
+++ mpause.rb
@@ -0,0 +1,29 @@
+#!/usr/local/bin/macruby
+
+framework "ScriptingBridge"
+
+system_events = SBApplication.applicationWithBundleIdentifier 'com.apple.systemevents'
+running_apps = system_events.processes.map(&:name)
+
+if running_apps.include? 'Spotify'
+  spotify = SBApplication.applicationWithBundleIdentifier 'com.spotify.client'
+  spotify.pause
+end
+
+if running_apps.include? 'Google Chrome'
+  chrome = SBApplication.applicationWithBundleIdentifier 'com.google.chrome'
+  pandora_tab = nil
+  chrome.windows.each do |window|
+    window.tabs.each do |tab|
+      if tab.URL =~ /pandora.com/
+        pandora_tab = tab
+      end
+    end
+  end
+
+  play_pause_js = <<-EOJS
+  $('.pauseButton:visible:first').click();
+  EOJS
+
+  pandora_tab && pandora_tab.executeJavascript(play_pause_js)
+end
---
+++ mplay.rb
@@ -0,0 +1,37 @@
+#!/usr/local/bin/macruby
+
+framework "ScriptingBridge"
+
+system_events = SBApplication.applicationWithBundleIdentifier 'com.apple.systemevents'
+running_apps = system_events.processes.map(&:name)
+
+if running_apps.include? 'Spotify'
+  spotify = SBApplication.applicationWithBundleIdentifier 'com.spotify.client'
+
+  if spotify.currentTrack.id
+    spotify.play
+    exit
+  end
+end
+
+if running_apps.include? 'Google Chrome'
+  chrome = SBApplication.applicationWithBundleIdentifier 'com.google.chrome'
+
+  pandora_tab = nil
+  chrome.windows.each do |window|
+    window.tabs.each do |tab|
+      if tab.URL =~ /pandora.com/
+        pandora_tab = tab
+      end
+    end
+  end
+
+  play_pause_js = <<-EOJS
+  $('.playButton:visible:first').click();
+  EOJS
+
+  if pandora_tab
+    pandora_tab.executeJavascript(play_pause_js)
+    exit
+  end
+end
